3a2b3935d22049de1eb39418b779780b88065806
Gregor Kralik <g.kralik@gmail.com>
2015-10-30 10:53:37 +0100

    remove data url prefix, return only base64 data string

--- main.js
+++ main.js
@@ -24,7 +24,22 @@ function grabScreenshot(offsetX, offsetY, height, width) {
         throw new Error('Unable to draw window: ' + e);
     }
 
-    return canvas.toDataURL();
+    try {
+        var dataUrl = canvas.toDataURL('image/png');
+    } catch(e) {
+        throw new Error('Unable to load canvas data into base64 string: ' + e);
+    }
+
+    var index = dataUrl.indexOf('base64,');
+    if (index == -1) {
+        throw new Error('Invalid base64 data: ' + dataUrl);
+    }
+
+    try {
+        return dataUrl.substring(index + 'base64,'.length);
+    } catch(e) {
+        throw new Error('Unable to extract data from base64 string: ' + e);
+    }
 }
 
 tabs.on('ready', function(tab) {
